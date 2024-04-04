402203d5c7e1da91224871f81124cec8d86a8519
Harsha70 <root@Harshavardhans-MacBook-Air.local>
2024-04-04 10:42:38 -0500

    added jwt token at sign up

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,5 @@
 const User = require("./../models/userModel");
-
+const jwt = require("jsonwebtoken");
 const catchAsync = (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch((err) => next(err)); // next(err) triggers the err middleware
@@ -7,10 +7,21 @@ const catchAsync = (fn) => {
 };
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+
+  //   console.log(process.env.JWT_EXPIRES_IN, process.env.JWT_SECRET);
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
 
   res.status(201).json({
     status: "success",
+    token,
     data: {
       user: newUser,
     },

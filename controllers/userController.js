6989e5fcb09fba4b22aac3a7e68b4558d018e30c
Harsha70 <root@Harshavardhans-MacBook-Air.local>
2024-04-06 20:50:14 -0500

    updateMe route

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,18 @@
 const User = require("../models/userModel");
+const AppError = require("../utils/appError");
 
 const catchAsync = (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch((err) => next(err)); // next(err) triggers the err middleware
   };
+};
+
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
 };
 
 exports.getAllUsers = catchAsync(async (req, res, next) => {
@@ -13,6 +22,31 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
     results: users.length,
     data: {
       users,
+    },
+  });
+});
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  // 1) Create error if user POSTs password data
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        "This route is not for password updates. Please use / updateMyPassword",
+        400
+      )
+    );
+  }
+  // 2) Update user document
+  const filteredBody = filterObj(req.body, "name", "email");
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      user: updatedUser,
     },
   });
 });

aab3db8cec60062c774bf91a90e37549fc344588
Harsha70 <root@Harshavardhans-MacBook-Air.local>
2024-04-07 12:10:40 -0500

    data sanitization

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const morgan = require("morgan");
 const rateLimit = require("express-rate-limit");
 const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
 
 const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
@@ -27,6 +29,11 @@ const limiter = rateLimit({
 app.use("/api", limiter);
 
 app.use(express.json());
+// Data sanitization against NOSQL query injection
+app.use(mongoSanitize());
+// Data sanitization against XSS
+app.use(xss());
+
 app.use(express.static(`${__dirname}/public`)); // can access the public directory
 
 app.use((req, res, next) => {

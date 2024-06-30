e615fb5533bf169f1653a36c35584b0cce31aef2
M.Harsha vardhan reddy <manyamharsha112@gmail.com>
2024-06-29 19:08:36 -0500

    Update logout.js

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,10 +1,10 @@
-const Produrl = "https://naturetour.onrender.com/api/v1/users/logout";
+const Prodlogouturl = "https://naturetour.onrender.com/api/v1/users/logout";
 const logout = async () => {
   try {
     const res = await axios({
       method: "GET",
       // url: "http://localhost:3000/api/v1/users/logout",
-      url:Produrl,
+      url:Prodlogouturl,
     });
     console.log(res);
     if ((res.data.status = "success")) location.reload(true);
